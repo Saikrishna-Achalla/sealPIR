dcca433a81c6ce34cedf1c87d277aaee3777ad5f
Saikrishna Achalla <saikrishnaachalla@Saikrishnas-MBP.lan>
2024-12-11 08:37:31 -0800

    First commit

--- src/pir_server.cpp
+++ src/pir_server.cpp
@@ -11,7 +11,7 @@ PIRServer::PIRServer(const EncryptionParameters &enc_params,
       is_db_preprocessed_(false) {
   context_ = make_shared<SEALContext>(enc_params, true);
   evaluator_ = make_unique<Evaluator>(*context_);
-  xencoder_ = make_unique<BatchEncoder>(*context_);
+  encoder_ = make_unique<BatchEncoder>(*context_);
 }
 
 void PIRServer::preprocess_database() {
